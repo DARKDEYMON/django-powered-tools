5115a739c1939bf8e9b1b84281cd149a82d32b96
Reynaldo Pereira heredia <darkdeymon04@gmail.com>
2025-03-20 18:38:08 -0400

    canbio de nombre a powered

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-	name = 'django-power-tools',
+	name = 'django-powered-tools',
 	version = '1.5',
-	packages = ['djangopowertools'],
-	description = 'View\'s extra para django que permiten busqueda sobre listas y redireccion con anclas',
+	packages = ['djangopoweredtools'],
+	description = 'View\'sv y herramientas extra para django que permiten busqueda sobre listas y redireccion con anclas y extras',
 	long_description = open('README.md', encoding='utf-8').read(),
 	long_description_content_type = 'text/markdown',
 	author = 'darkdeymon',
 	author_email = 'darkdeymon04@gmail.com',
-	url = 'https://github.com/DARKDEYMON/django-power-tools',
+	url = 'https://github.com/DARKDEYMON/django-powered-tools',
 	install_requires = [
 		'django>=5.1'
 	]
